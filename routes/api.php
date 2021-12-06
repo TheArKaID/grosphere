73c5ebd57627ee075dffabc2c8e2492239ced3bb
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-06 09:07:59 +0700

    Initial auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +20,18 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 });
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('user', function (Request $request) {
+        $user = $request->user();
+        $roles = $user->getRoleNames();
+        unset($user->roles);
+        $user->roles = $roles;
+
         return response()->json([
             'status' => 200,
             'message' => 'success',
-            'data' => $request->user(),
+            'data' => $user
         ], 200);
     });
 
