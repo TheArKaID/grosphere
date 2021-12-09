e471b315d5198ce62f6113e7c4c649680ea5aec2
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-09 14:45:40 +0700

    Adding phone to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,21 +4,43 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $email_verified_at
+ * @property string $password
+ * @property boolean $status
+ * @property string $remember_token
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Admin[] $admins
+ * @property Parent[] $parents
+ * @property Student[] $students
+ * @property Tutor[] $tutors
+ */
 class User extends Authenticatable
 {
     use HasRoles, HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
      */
-    protected $fillable = ['name', 'email', 'status', 'password'];
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'phone', 'email_verified_at', 'password', 'status', 'remember_token', 'created_at', 'updated_at'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -31,11 +53,32 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
-     *
-     * @var array
+     * User has One Detail
+     * 
+     * @return HasOne
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function detail()
+    {
+        $detail = null;
+
+        switch ($this->roles()->first()->name) {
+            case 'admin':
+                $detail = $this->hasOne(Admin::class);
+                break;
+            case 'parent':
+                $detail = $this->hasOne(Parent::class);
+                break;
+            case 'student':
+                $detail = $this->hasOne(Student::class);
+                break;
+            case 'tutor':
+                $detail = $this->hasOne(Tutor::class);
+                break;
+            default:
+                $detail = $this->hasOne(Student::class);
+                break;
+        }
+
+        return $detail;
+    }
 }
