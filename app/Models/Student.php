c9ff755b40d0667278710b3fad21a9be822a5a48
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-09 14:45:20 +0700

    Creating Admin, Parents, Student, Tutor Model and Table

---
+++ app/Models/Student.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $parent_id
+ * @property string $id_number
+ * @property string $birth_date
+ * @property string $birth_place
+ * @property boolean $gender
+ * @property string $address
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Parent $parent
+ * @property User $user
+ */
+class Student extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['user_id', 'parent_id', 'id_number', 'birth_date', 'birth_place', 'gender', 'address', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Parent');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
