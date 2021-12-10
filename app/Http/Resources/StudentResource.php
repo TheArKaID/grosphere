d73b9a1464d1eca9fc65b622ce0ad6f009e7e921
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-10 10:16:53 +0700

    Creating Student Resource and Collection

---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'phone' => $this->phone,
+            'gender' => $this->gender,
+            'created_at' => $this->created_at
+        ];
+    }
+}
