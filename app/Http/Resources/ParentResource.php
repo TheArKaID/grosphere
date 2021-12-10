6ba548d474772f7606a5241cc45c842b6d296bc5
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-10 16:38:22 +0700

    Creating Parent Resource and Collection

---
+++ app/Http/Resources/ParentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ParentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return $this->resource ? [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'students' => StudentResource::collection($this->whenLoaded('students')),
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'status' => $this->user->status,
+            'created_at' => $this->created_at
+        ] : [];
+    }
+}
