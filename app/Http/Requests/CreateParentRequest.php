f42fe4e9602830ced1a2eac19655a857840bec42
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-10 16:38:45 +0700

    Creating Create ParentRequest

---
+++ app/Http/Requests/CreateParentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateParentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'phone' => 'nullable|string|max:15|start_with:+628',
+            'address' => 'required'
+        ];
+    }
+}
