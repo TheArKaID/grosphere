b2e76edd3c7afab1bc8b50d2b77c441ac41f1d48
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-15 09:14:35 +0700

    Creating Tutor Resource Collection

---
+++ app/Http/Resources/TutorResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TutorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return $this->resource ? [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'status' => $this->user->status,
+            'created_at' => $this->created_at
+        ] : [];
+    }
+}
