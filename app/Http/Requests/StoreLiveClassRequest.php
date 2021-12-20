a9d0aa37c7c745cdeb04a3e21867f4209fd705f1
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-20 16:03:15 +0700

    Creating LiveClass Request for Storing & Updating

---
+++ app/Http/Requests/StoreLiveClassRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreLiveClassRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'tutor_id' => 'required|exists:tutors,id',
+            'name' => 'required|string|max:255',
+            'thumbnail' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'description' => 'required|string',
+            'duration' => 'required|integer',
+            'start_time' => 'required|date_format:Y-m-d H:i:s'
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateLiveClassRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateLiveClassRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'thumbnail' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'description' => 'required|string',
+            'duration' => 'required|integer',
+            'start_time' => 'required|date_format:Y-m-d H:i:s'
+        ];
+    }
+}
