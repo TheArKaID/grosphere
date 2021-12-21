33953d52f3ebd9e1b8d9356fad58cc49490ab49e
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-21 10:56:15 +0700

    Updating LiveClassResource

--- app/Http/Resources/LiveClassResource.php
+++ app/Http/Resources/LiveClassResource.php
@@ -14,6 +14,21 @@ class LiveClassResource extends JsonResource
      */
     public function toArray($request)
     {
-        return $this->resource ? parent::toArray($request) : [];
+        return $this->resource ? [
+            'id' => $this->id,
+            'duration' => $this->duration,
+            'start_time' => $this->start_time,
+            'class' => [
+                'id' => $this->class->id,
+                'name' => $this->class->name,
+                'description' => $this->class->description,
+                'thumbnail' => $this->class->thumbnail,
+                'type' => $this->class->type,
+                'tutor' => [
+                    'id' => $this->class->tutor->id,
+                    'name' => $this->class->tutor->user->name
+                ],
+            ],
+        ] : [];
     }
 }
