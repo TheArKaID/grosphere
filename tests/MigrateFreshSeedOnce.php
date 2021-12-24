dc89ff9cb15d64898b71aa72c634f1b876efe6de
Arifia Kasastra R <arka.progammer@gmail.com>
2021-12-24 15:22:49 +0700

    Creating Tutor, Student and Parent at tests

--- tests/MigrateFreshSeedOnce.php
+++ tests/MigrateFreshSeedOnce.php
@@ -2,6 +2,10 @@
 
 namespace Tests;
 
+use App\Models\Parents;
+use App\Models\Student;
+use App\Models\Tutor;
+use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
 
 trait MigrateFreshSeedOnce
@@ -11,6 +15,11 @@ trait MigrateFreshSeedOnce
      * @var boolean
      */
     protected static $setUpHasRunOnce = false;
+    /**
+     * If true, setup has run at least once.
+     * @var boolean
+     */
+    protected static $classSetUpHasRun = false;
 
     /**
      * After the first run of setUp "migrate:fresh --seed"
@@ -25,7 +34,31 @@ trait MigrateFreshSeedOnce
                 'db:seed',
                 ['--class' => 'DatabaseSeeder']
             );
+            $this->createDummyTutor();
+            $this->createDummyParent();
+            $this->createDummyStudent();
             static::$setUpHasRunOnce = true;
         }
+    }
+
+    public function createDummyTutor()
+    {
+        $user = User::factory()->create();
+        $user->assignRole('tutor');
+        Tutor::factory()->create(['user_id' => $user->id]);
+    }
+
+    public function createDummyStudent()
+    {
+        $user = User::factory()->create();
+        $user->assignRole('student');
+        Student::factory()->create(['user_id' => $user->id]);
+    }
+
+    public function createDummyParent()
+    {
+        $user = User::factory()->create();
+        $user->assignRole('parent');
+        Parents::factory()->create(['user_id' => $user->id]);
     }
 }
