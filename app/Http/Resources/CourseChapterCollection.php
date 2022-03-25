0c50ad91fb9bb5652ebf659bf1cc9651d5bed07f
Arifia Kasastra R <arka.progammer@gmail.com>
2022-03-25 09:10:19 +0700

    Creating Resource and Requres CourseChapter

---
+++ app/Http/Resources/CourseChapterCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CourseChapterCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
---
+++ app/Http/Resources/CourseChapterResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseChapterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        parent::wrap('course_chapters');
+
+        return $this->resource ? [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'order' => $this->order,
+            'status' => $this->status
+        ] : [];
+    }
+}
