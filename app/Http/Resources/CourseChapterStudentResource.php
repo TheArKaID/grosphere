9b541a8905a0b272db500c9bb3cff7f8356f7b89
Arifia Kasastra R <arka.progammer@gmail.com>
2022-03-25 14:08:49 +0700

    Creating Course Chapter Student Resource

---
+++ app/Http/Resources/CourseChapterStudentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseChapterStudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        parent::wrap('chapters');
+
+        $resource = $this->resource ? [
+            'id' => $this->id,
+            'title' => $this->title,
+            'order' => $this->order,
+            'status' => count($this->courseChapterStudents) > 0 ? $this->courseChapterStudents[0]->status : false,
+        ] : [];
+        $this->content ? $resource['content'] = $this->content : null;
+
+        return $resource;
+    }
+}
