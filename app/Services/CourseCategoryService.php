ef20b0fad0fae1eb3c1da11f6957d962d250e0a2
Arifia Kasastra R <arka.progammer@gmail.com>
2022-04-18 08:49:25 +0700

    Creating Course Category Service for admin

--- app/Services/CourseCategoryService.php
+++ app/Services/CourseCategoryService.php
@@ -39,4 +39,47 @@ class CourseCategoryService
     {
         return $this->courseCategory->findOrFail($id);
     }
+
+    /**
+     * Create Course Category
+     * 
+     * @param array $data
+     * 
+     * @return CourseCategory
+     */
+    public function create($data)
+    {
+        return $this->courseCategory->create($data);
+    }
+
+    /**
+     * Update Course Category
+     * 
+     * @param int $id
+     * @param array $data
+     * 
+     * @return CourseCategory
+     */
+    public function update($id, $data)
+    {
+        $courseCategory = $this->getById($id);
+        $courseCategory->update($data);
+
+        return $courseCategory;
+    }
+
+    /**
+     * Delete Course Category
+     * 
+     * @param int $id
+     * 
+     * @return CourseCategory
+     */
+    public function delete($id)
+    {
+        $courseCategory = $this->getById($id);
+        $courseCategory->delete();
+
+        return $courseCategory;
+    }
 }
