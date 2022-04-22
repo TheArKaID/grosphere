211d2dfb5430df517b70716292c2e6a28d03d731
Arifia Kasastra R <arka.progammer@gmail.com>
2022-04-22 13:06:38 +0700

    Creating Student Test and Student Test Answer

---
+++ database/migrations/2022_04_22_104410_create_student_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_tests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_chapter_student_id')->constrained();
+            $table->tinyInteger('status')->default(1);
+            $table->float('score')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_tests');
+    }
+}
