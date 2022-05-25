614114090bf1a740745a6169fb72ffae57781c71
Arifia Kasastra R <arka.progammer@gmail.com>
2022-05-25 08:38:57 +0700

    Creating method for tutor

--- app/Services/AskAnswerService.php
+++ app/Services/AskAnswerService.php
@@ -4,18 +4,19 @@ namespace App\Services;
 
 use App\Models\AskAnswer;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Auth;
 
 class AskAnswerService
 {
-    private $model, $courseStudentService;
+    private $model, $courseStudentService, $courseWorkService;
 
     public function __construct(
         AskAnswer $model,
-        CourseStudentService $courseStudentService
+        CourseStudentService $courseStudentService,
+        CourseWorkService $courseWorkService
     ) {
         $this->model = $model;
         $this->courseStudentService = $courseStudentService;
+        $this->courseWorkService = $courseWorkService;
     }
 
     /**
@@ -66,8 +67,60 @@ class AskAnswerService
     }
 
     /**
-     * Get Course Category
+     * Get all formatted Ask Answers for Tutor
      * 
+     * @param int $tutorId
+     * 
+     * @return Collection
+     */
+    public function getAllFormattedForTutor($tutorId)
+    {
+        $courseWorks = $this->courseWorkService->getAll($tutorId);
+
+        $askAnswers = [];
+        foreach ($courseWorks as $courseWork) {
+            $new = [
+                'course_work_id' => $courseWork->id,
+                'course_name' => $courseWork->class->name
+            ];
+            foreach ($courseWork->courseStudents as $cs) {
+                $new['course_student_id'] = $cs->id;
+                $new['student_name'] = $cs->student->user->name;
+                $new['unread'] = $cs->askAnswers()->where('updated_at', null)->where('from', AskAnswer::$FROM_STUDENT)->count();
+                $askAnswers[] = $new;
+            }
+        }
+
+        return $askAnswers;
+    }
+
+    /**
+     * Get One Formatted Ask Answer for Tutor
+     * 
+     * @param int $courseStudentId
+     * @param int $tutorId
+     * 
+     * @return Collection
+     */
+    public function getOneFormattedForTutor(int $courseStudentId, int $tutorId)
+    {
+        $courseStudent = $this->courseStudentService->getByIdAndTutorId($courseStudentId, $tutorId);
+        $askAnswers = $courseStudent->askAnswers->map(function ($askAnswer) {
+            if ($askAnswer->updated_at == null && $askAnswer->from == AskAnswer::$FROM_STUDENT) {
+                $askAnswer->updated_at = now();
+                $askAnswer->save();
+            }
+            return [
+                'from' => $askAnswer->from == 1 ? 'Student' : 'Tutor',
+                'message' => $askAnswer->message,
+                'time' => Carbon::parse($askAnswer->created_at)->format('d-m-Y H:i:s'),
+            ];
+        });
+
+        return $askAnswers;
+    }
+
+    /**
      * @param int $id
      * 
      * @return AskAnswer
@@ -89,6 +142,23 @@ class AskAnswerService
         $courseStudent = $this->courseStudentService->getByCourseIdAndStudentId($data['course_work_id'], $data['student_id']);
         $data['course_student_id'] = $courseStudent->id;
         $data['from'] = AskAnswer::$FROM_STUDENT;
+        $data['updated_at'] = null;
+
+        return $this->model->create($data);
+    }
+
+    /**
+     * Store Ask Answer for Tutor
+     * 
+     * @param array $data
+     * 
+     *  @return AskAnswer
+     */
+    public function storeForTutor(array $data)
+    {
+        $data['course_student_id'] = $data['course_student_id'];
+        $data['from'] = AskAnswer::$FROM_TUTOR;
+        $data['updated_at'] = null;
 
         return $this->model->create($data);
     }
