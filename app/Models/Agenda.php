95561a7906fcf1c2673f1fdf73fbba3a33cb7506
Arifia Kasastra R <arka.progammer@gmail.com>
2022-06-06 12:49:53 +0700

    Creating Agenda Model and table

---
+++ app/Models/Agenda.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $detail
+ * @property string $date
+ * @property string $created_at
+ * @property string $updated_at
+ * @property User $user
+ */
+class Agenda extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['user_id', 'detail', 'date', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
