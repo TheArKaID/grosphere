4934d0a0ef122671f69ea482b661e4417e327146
Arifia Kasastra R <arka.progammer@gmail.com>
2022-06-06 12:50:03 +0700

    Creating Agenda Service

---
+++ app/Services/AgendaService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Agenda;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+
+class AgendaService
+{
+    private $agenda;
+
+    public function __construct(Agenda $agenda)
+    {
+        $this->agenda = $agenda;
+    }
+
+    /**
+     * Get all Agendas
+     * 
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getAll()
+    {
+        if (request()->has('date')) {
+            $date = request()->get('date');
+            $this->agenda = $this->dateFilter($date);
+        }
+
+        return $this->agenda->where('user_id', Auth::user()->id)->get();
+    }
+
+    /**
+     * Date FIlter
+     * 
+     * @param string $date
+     * 
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function dateFilter($date)
+    {
+        return $this->agenda->whereDate('date', $date);
+    }
+
+    /**
+     * Create Agenda
+     * 
+     * @param array $data
+     * 
+     * @return \App\Models\Agenda
+     */
+    public function create($data)
+    {
+        $agenda = new Agenda();
+        $agenda->user_id = Auth::user()->id;
+        $agenda->date = $data['date'];
+        $agenda->detail = $data['detail'];
+        $agenda->save();
+
+        return $agenda;
+    }
+
+    /**
+     * Delete Agenda
+     * 
+     * @param int $agendaId
+     * 
+     * @return \App\Models\Agenda
+     */
+    public function delete($agendaId)
+    {
+        $agenda = $this->agenda->where('user_id', Auth::user()->id)->findOrFail($agendaId);
+        $agenda->delete();
+
+        return $agenda;
+    }
+}
