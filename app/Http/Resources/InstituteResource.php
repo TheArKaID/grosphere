d656c7177906992c834c14cccf312187b176306d
Arifia Kasastra R <arka.progammer@gmail.com>
2022-06-28 12:46:46 +0700

    Create Institute Resource

---
+++ app/Http/Resources/InstituteResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InstituteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        parent::wrap('institutes');
+
+        return $this->resource ? [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => $this->address,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'website' => $this->website,
+            'about' => $this->about,
+        ] : [];
+    }
+}
