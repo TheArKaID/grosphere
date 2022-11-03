687a8e2e23b9b023f047717e06497d95ca6a0f88
Arifia Kasastra R <arka.progammer@gmail.com>
2022-11-03 15:34:46 +0700

    Creating Group Resource Collection

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        parent::wrap('groups');
+        return $this->resource ? [
+            'id' => $this->id,
+            'name' => $this->name,
+            'students' => $this->students ? $this->students->map(function ($student) {
+                return [
+                    'id' => $student->id,
+                    'name' => $student->user->name,
+                    'email' => $student->user->email,
+                ];
+            }) : [],
+        ] : [];
+    }
+}
