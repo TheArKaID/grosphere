aa0f8588beae5521dc842d3d2c388bb99b102a6c
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-12 14:24:49 +0700

    chore: Add file size parsing method to TeacherFile model

--- app/Models/TeacherFile.php
+++ app/Models/TeacherFile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -31,5 +32,17 @@ class TeacherFile extends Model
     public function teacher()
     {
         return $this->belongsTo(Teacher::class);
+    }
+
+    /**
+     * Parse the file_size from bytes to mega bytes
+     * 
+     * @return string
+     */
+    public function fileSize(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => round($value / 1024 / 1024, 2)
+        );
     }
 }
