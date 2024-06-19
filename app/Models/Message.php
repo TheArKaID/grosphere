220c86d857d05ccec42e06a8748e1124723155e4
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-19 10:17:09 +0700

    feat: Add Message model and migration with UUID primary key and foreign keys for sender and recipient

---
+++ app/Models/Message.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $sender_id
+ * @property string $recipient_id
+ * @property string $message
+ * @property string $created_at
+ * @property string $updated_at
+ * @property User $user
+ * @property User $user
+ */
+class Message extends Model
+{
+    use HasUuids;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * 
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['sender_id', 'recipient_id', 'message', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'recipient_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
