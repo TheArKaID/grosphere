69cefb74aa856a33e9169dc8266eeac64faaf28a
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-19 11:21:51 +0700

    feat: Add MessageService to handle message recipients

---
+++ app/Services/MessageService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\MessageException;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
+
+class MessageService
+{
+    /**
+     * Get Users are allowed to send messages to
+     * 
+     * @param string|null $userId
+     * 
+     * @return Collection
+     */
+    public function getRecipients(string $userId = null): Collection
+    {
+        if (!$userId) {
+            $userId = Auth::id();
+        }
+
+        $user = User::find($userId);
+
+        switch ($user->roles()->first()?->name) {
+            case 'superadmin':
+                return User::whereHas('roles', function ($query) {
+                    $query->whereIn('name', ['admin']);
+                });
+                break;
+            case 'admin':
+                return User::whereHas('roles', function ($query) {
+                    $query->whereIn('name', ['superadmin', 'guardian', 'student', 'teacher']);
+                })->get();
+                break;
+            case 'guardian':
+                return User::whereHas('roles', function ($query) {
+                    $query->whereIn('name', ['admin', 'teacher']);
+                });
+                break;
+            case 'student':
+                return User::whereHas('roles', function ($query) {
+                    $query->whereIn('name', ['teacher']);
+                });
+                break;
+            case 'teacher':
+                return User::whereHas('roles', function ($query) {
+                    $query->whereIn('name', ['admin', 'student', 'parent']);
+                });
+                break;
+            default:
+                throw new MessageException('User not found');
+        }
+    }
+}
