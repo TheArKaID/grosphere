0b83db1a34e75d1b4d67ec05961b582293d89b2c
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-29 21:11:16 +0700

    Refactor AttendanceService to include timestamp in the generated proof file name

--- app/Services/AttendanceService.php
+++ app/Services/AttendanceService.php
@@ -129,7 +129,7 @@ class AttendanceService
         DB::beginTransaction();
 
         $proof = $data['proof'];
-        $fileName = 'attendances/' . $data['student_id'] . '_' . now()->format('Y-m-d') . '_' . $data['type'] . '.' . explode('/', explode(':', substr($proof, 0, strpos($proof, ';')))[1])[1];
+        $fileName = 'attendances/' . $data['student_id'] . '_' . now()->format('Y-m-d h:i:s') . '_' . $data['type'] . '.' . explode('/', explode(':', substr($proof, 0, strpos($proof, ';')))[1])[1];
 
         $data['proof'] = '';
         $data['admin_id'] = auth()->user()->detail->id;
