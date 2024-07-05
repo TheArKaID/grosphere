8fcfd621cfa5b9721b6208caf2ea222d837391b8
Arifia Kasastra R <arka.progammer@gmail.com>
2024-07-05 14:34:23 +0700

    feat: Update MessageSenderResource to handle null photo values

--- app/Http/Resources/MessageSenderResource.php
+++ app/Http/Resources/MessageSenderResource.php
@@ -21,7 +21,7 @@ class MessageSenderResource extends JsonResource
             'id' => $user?->id ?? '-',
             'name' => $user?->first_name ? $user?->first_name . ' ' . $user?->last_name : $user?->name,
             'role' => $user?->first_name ? $user?->roles()->first()->name : ($user?->user_id ? 'broadcast' : 'class-group'),
-            'photo' => $user?->first_name ? $this->getPhoto() : '-',
+            'photo' => $user?->first_name ? $this->getPhoto() : null,
             'message' => $this['message'] ? $this['message']->message : 'No message yet',
             'message_date' => $this['message']?->created_at?->format('Y-m-d H:i:s') ?? $user?->created_at->format('Y-m-d H:i:s'),
             'unread' => $this['unread']
